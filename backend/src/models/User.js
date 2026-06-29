9a1d1fbdae5acc4aef1c2823f98849539281d1c3
Sudesh Hansika <sudeshhansika@gmail.com>
2026-06-29 22:37:52 +0530

    feat: add JWT authentication with register, login, and protected /me route

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,9 +18,23 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
+      minlength: 6,
     },
   },
   { timestamps: true }
 );
+
+// Hash password before saving, but only if it changed
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Instance method to compare entered password with hashed one
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
